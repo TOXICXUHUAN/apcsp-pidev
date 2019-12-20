b9da029528e02a555dc249dfd041e048d1611744
Dylan Chen <cheny2408@my.sduhsd.net>
2019-12-20 11:54:33 -0800

    Real

--- variables.c
+++ variables.c
@@ -12,5 +12,28 @@ int main()
   tmp = c;
   c = a;
   a = tmp;
+  float d;
+  double e;
+  char f;
+  unsigned int g;
+  long int h;
+  int div = 3;
   printf("a, b and c are now: %d, %d, %d\n", a, b ,c);
+  printf("Int value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("Float value: %d and size: %d bytes\n", d, sizeof(d));
+  printf("Double value: %d and size: %d bytes\n", e, sizeof(e));
+  printf("Char value: %d and size: %d bytes\n", f, sizeof(f));
+  printf("Unsigned int value: %d and size: %d bytes\n", g, sizeof(g));
+  printf("Long int value: %d and size: %d bytes\n", h, sizeof(h));
+  if (a == 5)
+	{
+	printf("a is equal to 5\n");
+	}
+  for (int i=1; i<=100; i++)
+  	{
+	if ((i % div) == 0)
+        {
+        printf("%d\n", i);
+  	}
+}
 }
